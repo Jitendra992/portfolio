1559da2bbfcba952e41e5aa51589d8b6ed2ac396
Jitendra992 <jitendra8878singh@gmail.com>
2025-02-04 17:52:01 +0530

    commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const { closeMessage } = require("amqplib/lib/format")
+
+console.log("hii")
+alert("hii")
+
+
+
+ const helloo{
+
+
+
+ }
+
+
+ const hi=(id,id2)=>{
+
+
+    alert(id)
+
+
+
+
+ }
+
+
+ 
